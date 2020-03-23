ab731640347d8f954addab1aa2918fb19f134f6a
Corina <corinacvp@gmail.com>
2020-03-23 07:58:01 -0300

    Updates

---
+++ precache-manifest.dae5d8fbd1cf7390e97b8b43b01522a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc6982208de2524049fc15879c31e4a",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "2eaf3742289745eaf5a3",
+    "url": "/Portafolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "69bd98af8aaa843353af",
+    "url": "/Portafolio/static/css/main.45324c67.chunk.css"
+  },
+  {
+    "revision": "2eaf3742289745eaf5a3",
+    "url": "/Portafolio/static/js/2.e78ce577.chunk.js"
+  },
+  {
+    "revision": "405b76d2985863da411dfc6218976e45",
+    "url": "/Portafolio/static/js/2.e78ce577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69bd98af8aaa843353af",
+    "url": "/Portafolio/static/js/main.3015e3d7.chunk.js"
+  },
+  {
+    "revision": "1e19020dde68d02f111f",
+    "url": "/Portafolio/static/js/runtime-main.6b964254.js"
+  },
+  {
+    "revision": "27ae5002b17c05df6b619cfd26b27456",
+    "url": "/Portafolio/static/media/corina.27ae5002.JPG"
+  },
+  {
+    "revision": "4b9d6e0aeaf910dc3e4627afce6f857b",
+    "url": "/Portafolio/static/media/fondoPortafolio.4b9d6e0a.png"
+  }
+]);
