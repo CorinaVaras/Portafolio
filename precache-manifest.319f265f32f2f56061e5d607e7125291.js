14118cf4e235151221ced1013582d2cfcd013785
Corina <corinacvp@gmail.com>
2020-06-06 21:07:33 -0400

    Updates

---
+++ precache-manifest.319f265f32f2f56061e5d607e7125291.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf563cd97a7d02091bc6f0591a6ee6b8",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "b568f5af0b337e1e6d13",
+    "url": "/Portafolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2044126e09b161a69edf",
+    "url": "/Portafolio/static/css/main.10f74643.chunk.css"
+  },
+  {
+    "revision": "b568f5af0b337e1e6d13",
+    "url": "/Portafolio/static/js/2.e54914c6.chunk.js"
+  },
+  {
+    "revision": "857579e8f165266437adb86e4bb339e8",
+    "url": "/Portafolio/static/js/2.e54914c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2044126e09b161a69edf",
+    "url": "/Portafolio/static/js/main.3613ae88.chunk.js"
+  },
+  {
+    "revision": "1e19020dde68d02f111f",
+    "url": "/Portafolio/static/js/runtime-main.6b964254.js"
+  },
+  {
+    "revision": "27ae5002b17c05df6b619cfd26b27456",
+    "url": "/Portafolio/static/media/corina.27ae5002.JPG"
+  },
+  {
+    "revision": "c6a142cb1fb5025b489b7f9e4e9345c5",
+    "url": "/Portafolio/static/media/here.c6a142cb.png"
+  },
+  {
+    "revision": "059ada6006a1eca476309b6c5a1589d0",
+    "url": "/Portafolio/static/media/lol.059ada60.png"
+  },
+  {
+    "revision": "0228ffdbe8973df2c67a28595740a22f",
+    "url": "/Portafolio/static/media/redsocial.0228ffdb.svg"
+  },
+  {
+    "revision": "0a15a2b370fb0ef69fbdaf71e0de33fe",
+    "url": "/Portafolio/static/media/tenpo.0a15a2b3.svg"
+  }
+]);
