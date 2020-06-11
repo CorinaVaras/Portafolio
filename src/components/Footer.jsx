09fc57110e29553a4813c2e488bb44e1cee2493f
Corina <corinacvp@gmail.com>
2020-06-11 18:20:46 -0400

    add link cv

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,22 @@ const Footer = () => {
         <div className='container-footer' id='footer'>
 
             <div className="social-networks">
-            <a href="https://github.com/CorinaVaras" target="_blank" rel="noopener noreferrer">
+            <a href="https://drive.google.com/file/d/1DnYXG4MtyEj_hN39twaOanb-kzqPWt8V/view?usp=sharing" target="_blank" title='CV' rel="noopener noreferrer">
+              <span  className="fas fa-file-pdf" aria-hidden="true"></span>
+            </a>
+            
+            <a href="https://github.com/CorinaVaras" target="_blank" title='Conoce mis proyectos' rel="noopener noreferrer">
               <span className="fab fa-github fa-fw" aria-hidden="true"></span>
             </a>{" "}
-            <a href="https://www.linkedin.com/in/corinavaras/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/corinavaras/" target="_blank" title='Perfil profesional' rel="noopener noreferrer">
               <span className="fab fa-linkedin fa-fw" aria-hidden="true"></span>
             </a>{" "}
-            <a href="mailto:corinacvp@gmail.com">
+            <a href="mailto:corinacvp@gmail.com" target="_blank" title='Envíame un correo' rel="noopener noreferrer">
               <span className="fa fa-envelope-o fa-fw" aria-hidden="true"></span>
             </a>
+            
           </div>
+          
 
         <p>© 2020 Corina Varas Developer</p> 
             
